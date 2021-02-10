8b4550834fe379ef4162cee7e1c5475c355befd5
Alex Cortez <acim650@gmail.com>
2021-02-10 08:25:42 -0800

    api.js

--- api.js
+++ api.js
@@ -1,13 +1,25 @@
 
 const arrows = document.getElementById('arrows');
+const imageArrow = document.getElementById('image-arrow');
+const arrowsTwo = document.getElementById('arrows-2');
+
 
 arrows.addEventListener('click', () => {
-    expand();
+    work();
+});
+
+arrows.addEventListener('click', () => {
+    works();
 })
 
- function expand() {
-     arrows.style.transform = "scale(1.2)";
-     arrows.style.transition = "0.5s";
- }
+function work() {
+    arrows.style.transform = "scale(1.2)";
+    arrows.style.transition = "0.5s";
+}
 
+function works() {
+    arrowsTwo.style.transform = "scale(1.2)";
+    arrowsTwo.style.transition = "0.5s";
+}
 
+// Do clicked
