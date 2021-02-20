8777d5c47d2e5485a21289a4c3b77982d1f97dc9
Alex Cortez <acim650@gmail.com>
2021-02-20 10:09:42 -0800

    api.js

--- api.js
+++ api.js
@@ -7,18 +7,20 @@ const image = document.getElementById('getImage');
 const header = document.getElementById('thisone');
 
 var counter = 0;
-
-var images = [
-    "curry.jpg",
-    "oubre.jpg",
-    "wiggins.jpeg",
-    "green.jpg",
-    "wiseman.jpg"
+var images = [ 
+    {src: "curry.jpg"}, 
+    {src: "oubre.jpg"}, 
+    {src: "wiggins.jpeg"}, 
+    {src: "green.jpg"}, 
+    {src: "wiseman.jpg"} 
 ];
+var loaded = 0;
+
 
 button.addEventListener('click', () => {
     addOne();
     loadImages();
+    nextStep();
 })
 
 
@@ -32,13 +34,25 @@ function addOne() {
 
 function loadImages() {
     for(var i = 0; i < images.length; i++) {
-        image.src = images[i];
+        var tmp = new Image;
+        tmp.onload = function() {
+            loaded++;if(loaded == images.length){nextStep();}};
+        }
+        image.src = images[i]
         i++;
     }
+
+function nextStep() {
+    console.log("everything loaded");
 }
 
 
-// Button doesn't work on first click and doesn't match with the images // james wiseman picture doesn't show up first time
+
+
+
+// james wiseman picture doesn't show up first time
+    // - It's not the picture's fault
+// button doesn't work on first click and doesn't match with the images
 // Install API
 // Make sure grey boxes correspond with change in image
 // Make colors and design nice
